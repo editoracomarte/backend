8861f1072257860b66a44a0cf5ddd4c8cebdae64
maysaclaudino <maysa.silva@jusbrasil.com.br>
2026-06-22 14:55:33 -0300

    refactor: extract shuffle to testable util with unit tests

--- src/api/obra/services/obra.ts
+++ src/api/obra/services/obra.ts
@@ -4,14 +4,7 @@
 
 import { factories } from '@strapi/strapi';
 
-function shuffle<T>(arr: T[]): T[] {
-  const result = [...arr];
-  for (let i = result.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1));
-    [result[i], result[j]] = [result[j], result[i]];
-  }
-  return result;
-}
+import { shuffle } from '../../../utils/shuffle';
 
 export default factories.createCoreService('api::obra.obra', ({ strapi }) => ({
   async findFeatured() {
